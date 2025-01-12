b7c46ed263c4037bc19519925575f6ae9e3840bc
XXI <147558570+realxxi@users.noreply.github.com>
2025-01-13 00:09:31 +0500

    Genaerator web dasturi

---
+++ JS/app.js
@@ -0,0 +1,29 @@
+const form = document.querySelector("#form")
+const button = document.querySelector("#btn")
+
+const out = document.querySelector("#out")
+
+// const min = 1
+// const max = 10
+button.addEventListener("click", (e) => {
+    e.preventDefault()
+    const minInput = document.querySelector("#min")
+    const maxInput = document.querySelector("#max")
+    const min = Number(minInput.value)
+    const max = Number(maxInput.value)
+
+    if (min < max) {
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min 
+        out.textContent = randomNumber
+
+    }else if(min===max){
+        out.textContent ="error "
+    }
+})
+
+
+
+
+// console.log(max)
+
+//     console.log( randomNumber)
